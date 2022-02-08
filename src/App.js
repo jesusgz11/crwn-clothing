bbc8868df820130b02bfcd5d9b0669a36c7e3509
Jesus Guzman <jesusguzman.0311@gmail.com>
2022-02-08 14:09:06 -0600

    Collection Preview, Item and Shop Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 import { Routes, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
