0bc4434379d53e1c81ff684e547664b17d3c6d9e
Jesus Guzman <jesusguzman.0311@gmail.com>
2022-02-25 16:43:26 -0600

    Separated server to handle stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,28 @@
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
     'pk_test_51KUG2cG3I9OONYfoo5wHrAd6t3YpzDRjAd7P08gB11FiIJxjEU62aQYGy3ynRwPcVAvJXnEVrBcehwvTAeEi8cIj00XqJwHpVx';
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment Successful');
+      })
+      .catch((error) => {
+        console.log('Payment Error:', JSON.parse(error));
+        alert(
+          'There was an issue with your payment. Please make sure you use the provided credit card.'
+        );
+      });
   };
   return (
     <StripeCheckout
